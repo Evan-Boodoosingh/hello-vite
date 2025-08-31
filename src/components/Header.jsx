7ff94a83fc9e1e1f24ef3ec2b73b29e2e8e5ec1d
Evan Boodoosingh <evan9496@gmail.com>
2025-08-31 01:25:58 -0400

    Add initial components and styles for the Vite project
    - Created App component to serve as the main application structure.
    - Added Header component with logo and title.
    - Implemented Main component with introductory text.
    - Introduced App.css for styling, including layout and logo animations.

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import headerLogo from "../assets/logo.png"
+
+ function Header() {
+   return (
+     <div>
+        <img src={headerLogo} alt="tripleten logo" />
+       <h1>Hello Vite!</h1>
+     </div>
+   );
+ };
+ 
+ export default Header;
+ 
---
+++ src/components/Main.jsx
@@ -0,0 +1,7 @@
+ function Main() {
+   return (
+     <p>This is your first Vite project</p>
+   );
+ };
+ 
+ export default Main;
